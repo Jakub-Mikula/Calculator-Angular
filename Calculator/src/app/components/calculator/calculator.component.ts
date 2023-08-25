8aaf9c974f49d05dfbf39d202703f2976641c377
Jakub Mikula <jakub.mikula20@gmail.com>
2023-08-25 16:17:23 +0200

    Added calculate function to the calculator and changed logic of few add operator or number

--- Calculator/src/app/components/calculator/calculator.component.ts
+++ Calculator/src/app/components/calculator/calculator.component.ts
@@ -9,14 +9,18 @@ export class CalculatorComponent {
   input : string = "";
   counterLeft : number = 0;   //counter for left bracket (
   counterRight : number = 0; //counter for right bracket ')'
-  numberOfOperations : number = 1;
   numberOfDecimalPoints : 0 = 0;
 
 
+  result : string = ""; // result as a string
+  answer : number = 0; // answer as float
+  binaryAndHexadecimalAnswer : number = 0; // answer as int to be able to do toString(2) or to String(16) without to many characters
+
+
   addNumber(number : string) : void {
+    let lastKey = this.input[this.input.length-1]; // last char in input
+    let secondLastKey = this.input[this.input.length-2]; // second last char in input (used for ^2)
     if(number == "."){   //added '.' as a number, because it is more affecting number itself
-      let lastKey = this.input[this.input.length-1]; // last char in input
-      let secondLastKey = this.input[this.input.length-2]; // second last char in input (used for ^2)
       if (this.numberOfDecimalPoints > 0){  // do not want two '.' in one operation
         return;
       }else if(this.input == ""){
@@ -30,7 +34,7 @@ export class CalculatorComponent {
         this.numberOfDecimalPoints++;
         this.input = this.input + number;
       }
-    }else {
+    }else if(secondLastKey != "^"){  // this ensures that everything will be x^2 nothing more (x^3/x^26)
       this.input =  this.input + number;
     }
   }
@@ -47,7 +51,7 @@ export class CalculatorComponent {
     let secondLastKey = this.input[this.input.length-2]; // second last char in input (used for ^2)
     if (lastKey == "."){ //cannot use operator after "."
       return;
-    }else if((secondLastKey == "^") && (operator == "^2" || operator == "√")){  //cannot use ^2 and √ after ^2
+    }else if((secondLastKey == "^") && (operator == "√")){  //cannot use ^2 and √ after ^2
       return;
     }else if(lastKey === "/" || lastKey === "*" || lastKey === "-" || lastKey === "+" || lastKey == "(" || lastKey === "√"){
       //if last char is operator as well u cant continue
@@ -75,8 +79,34 @@ export class CalculatorComponent {
     }
   }
 
+  calculate(){
+    if (this.input == ""){
+      return;
+    }
+    let formula = this.input;
+    let lastKey = formula[formula.length - 1];
+    if (lastKey === '.')  {
+      formula=formula.substr(0,formula.length - 1);
+    }
+    lastKey = formula[formula.length - 1];
+    if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+' || lastKey === '.' || lastKey === '(')  {
+      formula=formula.substr(0,formula.length - 1);
+    }
+    formula = formula.replace(/\^2/g, '**2');
+    formula = formula.replace(/√/g, 'Math.sqrt');
+    console.log("Formula " + formula);
+    this.result = eval(formula);
+    console.log("Result " + this.result)
+    this.answer = parseFloat(this.result);
+    this.binaryAndHexadecimalAnswer = parseInt(this.result);
+
+    this.input = "";
+  }
+
   clear() : void {
     this.input = "";
+    this.answer = 0;
+    this.binaryAndHexadecimalAnswer = 0;
   }
 
   clearLast() : void{
